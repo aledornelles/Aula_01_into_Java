0de3fb28e8af2a0bc30ca339e1ca42f4e2498920
ALESSANDRO DORNELLES JÚNIOR <alessandro_dornel1@estudante.sesisenai.org.br>
2023-07-11 21:17:16 -0300

    primeira aula

---
+++ PrimeiraAula.java
@@ -0,0 +1,12 @@
+package primeiraaula;
+
+public class PrimeiraAula {
+
+    public static void main(String[] args) { //essa é a função
+        System.out.println("Hello World maluco");
+    }
+    
+    public void teste() {
+    
+    }
+}
